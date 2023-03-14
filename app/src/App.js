dcac59973379ef420760496115abddd245e5d4ad
Ahan <64554828+AhanR@users.noreply.github.com>
2023-03-14 12:29:33 +0530

    adding the basic layout

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [openResult, setOpenResult] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [content, setContent] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form action="" className={isError? "error":"normal"} onSubmit={(evt)=>{
+        evt.preventDefault();
+          // openResult if there is some content
+          // else set the error
+          if(content.length !== 0){
+            setOpenResult(true);
+          } else (
+            setIsError(true)
+          )
+      }}>
+        <textarea className="editable" value={content} onChange={(e)=>{
+          if(isError) setIsError(false);
+          setContent(e.target.value);
+        }}></textarea>
+        <button type="submit" className='submit-btn'>Submit</button>
+      </form>
+      {(openResult)?<div className="result">
+        <div className="display">{content}</div>
+        <button className='close-btn' onClick={(evt)=>{
+          setOpenResult(false);
+        }} >Close</button>
+      </div>:""}
+    </section>
   );
 }
 
