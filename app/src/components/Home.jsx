c1badfc0a62400a9fdcd5a923073a03cdfd5d442
Parth Prajapati <pp41495233@gmail.com>
2023-03-18 10:14:26 +0530

    update input field

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -6,12 +6,19 @@ import WarningIcon from "../assets/WarningIcon";
 
 const Home = () => {
   const [showModal, setShowModal] = useState(false);
+  const [isValid, setIsValid] = useState(false);
   const navigate = useNavigate();
   const onSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const text = formData.get("text").toString() ?? "";
+    let text = formData.get("text").toString() ?? "";
+    text = text.trim();
     if (text == "") {
+      setShowModal(true);
+      return;
+    }
+    if (text.includes(" ")) {
+      setIsValid(true);
       setShowModal(true);
       return;
     }
@@ -27,18 +34,26 @@ const Home = () => {
       <div className="home__main">
         <h3>Remove Duplicate Character</h3>
         <form onSubmit={onSubmit}>
-          <input id="text" placeholder="Enter the text" name="text" />
+          <input
+            id="text"
+            placeholder="Enter the text"
+            name="text"
+            pattern="[a-zA-Z ]*"
+            title="Please enter alphabets only."
+          />
           <button type="submit" className="btn btn--primary">
             Submit
           </button>
         </form>
       </div>
-      {showModal && <WarningModal setShowModal={setShowModal} />}
+      {showModal && (
+        <WarningModal setShowModal={setShowModal} isValid={isValid} />
+      )}
     </main>
   );
 };
 
-function WarningModal({ setShowModal }) {
+function WarningModal({ setShowModal, isValid }) {
   return (
     <Modal>
       <section className="warning__modal">
@@ -46,10 +61,11 @@ function WarningModal({ setShowModal }) {
           <WarningIcon />
         </span>
         <div>
-          <h4>No Input Error</h4>
+          <h4>{isValid ? "Invalid Input" : "No Input Error"}</h4>
           <p>
-            You can&apos;t submit empty text to the output page. Please input
-            some text.
+            {isValid
+              ? "You can't include space in the text. Please enter again"
+              : "You can't submit empty text to the output page. Please input some text."}
           </p>
           <button className="btn" onClick={() => setShowModal(false)}>
             Cancel
